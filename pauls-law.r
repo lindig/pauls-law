21b7612c19b71342b83d769d69fdb5c44424305b
Christian Lindig <lindig@gmail.com>
2025-08-06 21:27:32 +0100

    Use ggplot2 for graphics
    Signed-off-by: Christian Lindig <lindig@gmail.com>

--- pauls-law.r
+++ pauls-law.r
@@ -1,5 +1,7 @@
 #! /usr/bin/env Rscript
 
+library(ggplot2)
+
 df <- read.csv("pairs.csv")
 df$gender <- as.factor(df$gender)
 df$logd <-  log(df$dist2/df$dist1,2)
@@ -10,30 +12,32 @@ df$split2 <- df$time2/df$dist2*500
 df$delta <- df$split2 - df$split1
 df$paul <- df$delta/df$logd
 df$alpha <- log(df$time2/df$time1)/log(df$dist2/df$dist1)
- 
-png(filename = "paul.png", width = 800, height = 600, res = 100)
-hist(df$paul,
-     main = "Paul's Constant",
-     xlab = "Change over 500m (Seconds)",
-     ylab = "Frequency",
-     col = "skyblue",
-     border = "black",
-     breaks = 100,
-     freq = TRUE
-     )
-dev.off()
-png(filename = "power.png", width = 800, height = 600, res = 100)
-hist(df$alpha,
-     main = "Power Law",
-     xlab = "Constant Alpha",
-     ylab = "Frequency",
-     col = "skyblue",
-     border = "black",
-     breaks = 100,
-     freq = TRUE
-     )
-dev.off()
+
+mean_paul <- mean(df$paul)
+mean_alpha <- mean(df$alpha)
 
 summary(df$paul)
 summary(df$alpha)
+
+p1 <-
+  ggplot(data=df, aes(x = paul)) +
+  geom_histogram(bins=60, fill="skyblue") +
+  geom_vline(aes(xintercept = mean_paul)) +
+  labs(title = "Distribution of Paul's Constant",
+       subtitle = paste("Mean =", round(mean_paul, 2)),
+       x = "Constant",
+       y = "Frequency")
+ggsave("paul.png", p1)
+
+p2 <-
+  ggplot(data=df, aes(x = alpha)) +
+  geom_histogram(bins=60, fill="skyblue") +
+  geom_vline(aes(xintercept = mean_alpha)) +
+  labs(title = "Distribution of Constant Alpha",
+       subtitle = paste("Mean =", round(mean_alpha, 3)),
+       x = "Constant",
+       y = "Frequency")
+
+ggsave("power.png", p2)
+
 
